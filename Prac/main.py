5a952e06b04a2a667f573e1a1860847de0e702c4
ShreyesGadwalkar <shreyes.gadwalkar@gmail.com>
2022-05-08 21:57:09 -0400

    done

---
+++ Prac/main.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import sys
+from map import *
+from level import Level
+
+class Main:
+    def __init__(self):
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Fakèmon")
+        self.clock = pg.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+            
+            self.screen.fill('sky blue')
+            self.level.run()
+            self.clock.tick(FPS)
+            pg.display.update()
+
+if __name__ == '__main__':
+    Main().run()
---
+++ Prac/map.py
@@ -0,0 +1,10 @@
+#map setup
+HEIGHT = 1280
+WIDTH = 800
+FPS = 60
+TILESIZE = 32
+
+WORLD_MAP = [
+    ['x', 'x', 'x','x', 'x', 'x','x', 'x', 'x','x', 'x', 'x'],
+]
+
