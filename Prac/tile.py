f46cce1612cce6918a02011e0df72fbff8c80543
ShreyesGadwalkar <shreyes.gadwalkar@gmail.com>
2022-05-08 21:58:05 -0400

    day 1

--- Prac/tile.py
+++ Prac/tile.py
@@ -5,4 +5,4 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load()
-        self.rect = self.image.get_rect(topleft=pos)
+        self.rect = self.image.get_rect(topleft = pos)
