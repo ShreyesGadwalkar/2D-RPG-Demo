460f96c87379f330ff8eae5832e5ee553d330f83
ShreyesGadwalkar <shreyes.gadwalkar@gmail.com>
2022-05-12 15:15:39 +0530

    collision detection midway done

---
+++ Prac/player.py
@@ -0,0 +1,49 @@
+from nis import match
+import pygame
+from map import *
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load("Zelda/1 - level/graphics/test/player.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+        self.obstacle_sprites = obstacle_sprites
+
+    def collision(self, direction):
+        if(direction == "horizontal"):
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    
+
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+            self.direction.x = 0
+    
+    def move(self, speed):
+        if(self.direction.magnitude() != 0):
+            self.direction = self.direction.normalize()
+        self.rect.center += self.direction * speed
+    
+    
+
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
--- Prac/tile.py
+++ Prac/tile.py
@@ -4,5 +4,5 @@ from map import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load()
+        self.image = pygame.image.load("Zelda/1 - level/graphics/test/rock.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
